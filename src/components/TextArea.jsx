d97ae038fcd03f896cc439934fd39c6381f3fe42
EdlawitG <eduedil30@gmail.com>
2024-01-18 22:19:53 +0300

    feat:input-text_field

---
+++ src/components/TextArea.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const TextArea = ({ placeholder, value, onChange, rows, cols }) => {
+  return (
+    <textarea
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+      rows={rows}
+      cols={cols}
+      className="bg-F5F8FA rounded-small border-2 border-br_primary px-4 py-2 text-sm text-tx_secondary placeholder-tx_addtional focus:outline-none"
+    />
+  );
+};
+
+export default TextArea;
