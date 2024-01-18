1b49650688c6e5aa003e0ff7589aa6073f222832
Betelhemmesele <betimesele93@gmail.com>
2024-01-18 18:59:55 +0300

    style:prettier

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,23 +1,22 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-const Button = ({ children, color,size, ...props }) => {
-  let buttonSizeClass = '';
- 
-  if (size === 'small') {
-    buttonSizeClass = 'h-[26px] w-[74px] px-4 text-sm rounded-small';
-  } else if (size === 'large') {
-    buttonSizeClass = ' w-[470px] h-12 px-10 rounded-large';
+const Button = ({ children, color, size, ...props }) => {
+  let buttonSizeClass = "";
+
+  if (size === "small") {
+    buttonSizeClass = "h-[26px] w-[74px] px-4 text-sm rounded-small";
+  } else if (size === "large") {
+    buttonSizeClass = " w-[470px] h-12 px-10 rounded-large";
   } else {
-    buttonSizeClass = 'h-[60px] w-[206px] px-15 rounded-small';
+    buttonSizeClass = "h-[60px] w-[206px] px-15 rounded-small";
   }
 
   return (
     <button
-      className={`inline-flex items-center justify-center ${buttonSizeClass} gap-4 flex-shrink-0 flex-wrap border border-none bg-${color}
-       text-white hover:${color} hover:opacity-80 focus:outline-none  focus:ring-offset-0 focus:ring-meke-500`}
-      {...props}
-    >
+      className={`inline-flex items-center justify-center ${buttonSizeClass} flex-shrink-0 flex-wrap gap-4 border border-none bg-${color}
+       text-white hover:${color} hover:opacity-80 focus:outline-none  focus:ring-meke-500 focus:ring-offset-0`}
+      {...props}>
       {children}
     </button>
   );
@@ -25,12 +24,11 @@ const Button = ({ children, color,size, ...props }) => {
 
 Button.propTypes = {
   children: PropTypes.node.isRequired,
-  size: PropTypes.oneOf(['small', 'default', 'large']),
+  size: PropTypes.oneOf(["small", "default", "large"])
 };
 
 Button.defaultProps = {
-  size: 'default',
+  size: "default"
 };
 
 export default Button;
-
