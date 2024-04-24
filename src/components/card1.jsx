41355a930e5763e071ccc43fc5a7f6f34d8cfee8
Betelhem-hailu <betelhemhailu811@gmail.com>
2024-04-25 00:37:04 +0300

    fix

--- src/components/card1.jsx
+++ src/components/card1.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-
 const Card1 = ({ imageUrl, header, place }) => {
   const imageStyle = {
     width: "100%",
@@ -11,23 +10,23 @@ const Card1 = ({ imageUrl, header, place }) => {
   return (
     <div className="rounded-lg shadow-lg" style={cardStyle}>
       <img src={imageUrl} alt="Card Image" style={imageStyle} />
-    <div className="md:w-364 w-100 md:h-548 mb-5 ml-5 mr-11 mt-10 w-full rounded-lg shadow-lg">
-      <img src={imageUrl} alt="pho" style={imageStyle} />
-      <div
-        className="mt-[30px] flex flex-shrink-0 flex-col items-center justify-center"
-        style={{ width: "404px", height: "56px" }}>
-        <p
-          className="font-roboto text-2xl font-bold  text-black"
-          style={{ color: "#000", fontSize: "24px" }}>
-          {header}
-        </p>
-        <p
-          className="mb-3 font-roboto text-base font-light text-black"
-          style={{ color: "#000", fontSize: "20px" }}>
-          {place}
-        </p>
+      <div className="md:w-364 w-100 md:h-548 mb-5 ml-5 mr-11 mt-10 w-full rounded-lg shadow-lg">
+        <img src={imageUrl} alt="pho" style={imageStyle} />
+        <div
+          className="mt-[30px] flex flex-shrink-0 flex-col items-center justify-center"
+          style={{ width: "404px", height: "56px" }}>
+          <p
+            className="font-roboto text-2xl font-bold  text-black"
+            style={{ color: "#000", fontSize: "24px" }}>
+            {header}
+          </p>
+          <p
+            className="mb-3 font-roboto text-base font-light text-black"
+            style={{ color: "#000", fontSize: "20px" }}>
+            {place}
+          </p>
+        </div>
       </div>
-    </div>
     </div>
   );
 };
