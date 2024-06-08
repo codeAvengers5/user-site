6ef585af98683eef71afe75de15c8d630b96dc24
Edlawit G <eduedil30@gmail.com>
2024-06-08 20:59:09 +0300

    fix:register

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -21,13 +21,11 @@ const initialState = {
 const Registration = () => {
   const router = useRouter();
   const [users, setUser] = useState(initialState);
-  const [errors, setErrors] = useState("")
+  const [errors, setErrors] = useState({ error: null, username: "", email: "", password: "" })
   const dispatch = useDispatch();
   const onInputChange = e => {
     const { name, value } = e.target;
     setUser({ ...users, [name]: value });
-    const validationErrors = validate(users);
-    setErrors(validationErrors);
   };
 
   const { user, success, valid, isLoggedIn, error, msg } = useSelector(
@@ -36,22 +34,60 @@ const Registration = () => {
 
   useEffect(() => {
     if (error) {
-      toast.error("Registration Failed!");
-    } else if (success) {
-      toast.success("Registered successful!");
+      setErrors({ error: error })
+      toast.error(errors.error)
+      return;
     }
   }, [error, success]);
   const handleSubmit = async e => {
     e.preventDefault();
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!users.username) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        username: "Please enter your UserName.",
+      }));
+      return;
+    } 
+    if (!users.email) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        email: "Please enter your email.",
+      }));
+      return;
+    } else if (!emailRegex.test(users.email)) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        email: "Please enter a valid email.",
+      }));
+      return;
+    }
+    else {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        email: "",
+      }));
+    }
+    if (!users.password) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        password: "Please enter your password.",
+      }));
+      return;
+    } else {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        phoneNumber: "",
+      }));
+    }
     try {
-      // console.log("user", users);
       dispatch(register(users));
     } catch (error) {
       console.log(error);
     }
   };
   if (success) {
-    console.log("success");
+    // console.log("success");
     router.push("/confirmemail");
   }
 
@@ -69,6 +105,8 @@ const Registration = () => {
           <div>
             <p className="mb-[5px] font-secondary font-light">Username</p>
             <div className="rounded-6xs flex flex-row items-start justify-start self-stretch border-[1px] border-solid border-darksalmon bg-whitesmoke px-2.5 py-[15px]">
+            {errors.username && <div className={`text-[#E71D36] mb-2 mt-2}`}>
+              {errors.username}</div>}
               <input
                 className="relative inline-block h-[17px] w-full bg-[transparent] p-0 text-left font-secondary text-sm text-black [border:none] [outline:none]"
                 type="text"
@@ -77,16 +115,14 @@ const Registration = () => {
                 value={users.username}
                 onChange={onInputChange}
               />
-             
-            </div> <CustomErrorViewer
-              isShow={errors.email !== ""}
-              text={errors.email}
-            />
+
+            </div>
           </div>
           <div>
-            {" "}
             <p className="mb-[5px] font-secondary font-light">Email</p>
             <div className="rounded-6xs flex flex-row items-start justify-start self-stretch border-[1px] border-solid border-darksalmon bg-whitesmoke px-2.5 py-[15px]">
+            {errors.email && <div className={`text-[#E71D36] mb-2 mt-2}`}>
+              {errors.email}</div>}
               <input
                 className="relative inline-block h-[17px] w-full bg-[transparent] p-0 text-left font-secondary  text-sm text-black [border:none] [outline:none]"
                 type="text"
@@ -95,15 +131,14 @@ const Registration = () => {
                 value={users.email}
                 onChange={onInputChange}
               />
-             
-            </div> <CustomErrorViewer
-              isShow={errors.email !== ""}
-              text={errors.email}
-            />
+
+            </div>
           </div>
           <div>
             <p className="mb-[5px] font-secondary font-light">Password</p>
             <div className="rounded-6xs flex flex-row items-start justify-start self-stretch border-[1px] border-solid border-darksalmon bg-whitesmoke px-2.5 py-[15px]">
+            {errors.password && <div className={`text-[#E71D36] mb-2 mt-2}`}>
+              {errors.password}</div>}
               <input
                 className="relative inline-block h-[17px] w-full bg-[transparent] p-0 text-left font-secondary text-sm text-black [border:none] [outline:none]"
                 type="password"
@@ -112,17 +147,18 @@ const Registration = () => {
                 value={users.password}
                 onChange={onInputChange}
               />
-             
-            </div> {!users.password ? (
-                ""
-              ) : (
-                <PasswordChecklist
-                  className="text-sm"
-                  rules={["capital", "specialChar", "minLength", "number"]}
-                  minLength={8}
-                  value={users.password}
-                />
-              )}
+
+            </div> 
+            {/* {!users.password ? (
+              ""
+            ) : (
+              <PasswordChecklist
+                className="text-sm"
+                rules={["capital", "specialChar", "minLength", "number"]}
+                minLength={8}
+                value={users.password}
+              />
+            )} */}
           </div>
           <button className="box-border flex h-[50px] flex-row items-center justify-center rounded-[10px] bg-[#E71D36] p-2.5 font-secondary text-base text-white md:text-[24px]">
             Signup
